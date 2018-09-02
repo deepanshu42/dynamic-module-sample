f2d1a7b85aaa22cad21b2f2a4de8a44d18c94484
deepanshu42 <dsehgal37@gmail.com>
2018-09-03 03:10:19 +0530

    Initial commit

---
+++ app/src/main/java/com/test/dynamic/MainActivity.java
@@ -0,0 +1,94 @@
+package com.test.dynamic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.play.core.splitinstall.SplitInstallManager;
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
+import com.google.android.play.core.splitinstall.SplitInstallRequest;
+import com.google.android.play.core.splitinstall.SplitInstallSessionState;
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
+import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button download;
+    private Button start;
+
+    private int mySessionId;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        download = findViewById(R.id.download);
+        start = findViewById(R.id.start);
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadDynamicModule();
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName("com.test.dynamic", "com.test.dynamic.test.TestActivity");
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void downloadDynamicModule() {
+        SplitInstallManager splitInstallManager =
+                SplitInstallManagerFactory.create(this);
+
+        SplitInstallRequest request =
+                SplitInstallRequest
+                        .newBuilder()
+                        .addModule("test")
+                        .build();
+
+        SplitInstallStateUpdatedListener listener = new SplitInstallStateUpdatedListener() {
+            @Override
+            public void onStateUpdate(SplitInstallSessionState splitInstallSessionState) {
+                if(splitInstallSessionState.sessionId() == mySessionId) {
+                    switch (splitInstallSessionState.status()) {
+                        case SplitInstallSessionStatus.INSTALLED:
+                            Log.d(TAG, "Module downloaded");
+                    }
+                }
+            }
+        };
+
+        splitInstallManager.registerListener(listener);
+
+        splitInstallManager.startInstall(request)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.d(TAG, "Exception: " + e);
+                    }
+                })
+                .addOnSuccessListener(new OnSuccessListener<Integer>() {
+                    @Override
+                    public void onSuccess(Integer sessionId) {
+                        mySessionId = sessionId;
+                    }
+                });
+    }
+}
---
+++ test/src/main/java/com/test/dynamic/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.test.dynamic.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+}
